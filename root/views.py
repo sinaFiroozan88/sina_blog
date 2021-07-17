573a2207c9f21190dd7a5a3facc540c22cec4b94
sina <sinafiroozan@gmail.com>
2021-07-18 01:02:13 +0430

    Added views to create/edit blog post inside the site.

--- root/views.py
+++ root/views.py
@@ -1,13 +1,46 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from .import models
+from . import models
+from . forms import PostForm
 
 
 def post_list(request):
-    posts = models.Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = models.Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'root/post_list.html', {'posts': posts})
 
 
 def post_detail(request, pk):
     post = get_object_or_404(models.Post, pk=pk)
     return render(request, 'root/post_detail.html', {'post': post})
+
+
+@login_required(login_url='/admin/')
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm
+    return render(request, 'root/post_new.html', {'form': form})
+
+
+@login_required(login_url='/admin/')
+def post_edit(request, pk):
+    post = get_object_or_404(models.Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'root/post_new.html', {'form': form})
